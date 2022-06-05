8c2e67b88d8bdba86fefa052cbe384c40d8654b5
anderson <anderson.ap133@gmail.com>
2022-06-04 23:21:51 -0300

    Fetched Api Key From WeatherMap

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <h1 className="">App</h1>
     </div>
   );
 }
