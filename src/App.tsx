2b3c43ffcf489ce43115ec73f67d7ff1ae7cc252
anderson <anderson.ap133@gmail.com>
2022-06-05 23:49:24 -0300

    Finished Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="bg-gray-800 flex items-center justify-center w-screen h-screen py-10">
-      <div className="flex w-3/4 min-h-full rounded-3x1 shadow-lg m-auto bg-gray-100">
+      <div className="flex w-3/4 min-h-full rounded-3xl shadow-lg m-auto bg-gray-100">
 
        <div className='form-container'>
          <div className='flex items-center justify-center'>
@@ -27,13 +27,46 @@ function App() {
            </div>
          </div>
          <div className='flex flex-col items-center justify-center h-full '>
-           <h1 className='text-white text-2x1'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est tempore ea unde. Quo placeat error aperiam minus </h1>
+           <h1 className='text-white text-2xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est tempore ea unde. Quo placeat error aperiam minus </h1>
            <hr className='h-1 bg-white w-1/4 rounded-full my-5' />
            <form noValidate onSubmit={handleSubmit} className='flex justify-center w-full '>
-             <input />
+             <input
+             type='text'
+             className='relative rounded-xl py-2 py-3 w-2/3 bg-gray-300 bg-opacity-60 text-white placeholder-gray-200'
+             onChange={handleChange}
+             required />
+             <button type='submit' className='z-10'>
+               <i className='fa fa-search text-white ml-10 border-1 my-auto z-10 cursor-pointer p-3' aria-hidden='true' onClick={() => {
+                 navigator.geolocation.getCurrentPosition(myIP)
+               }}>
+               </i>
+             </button>
            </form>
          </div>
+       </div>
 
+
+       <div className='w-2/4 p-5'>
+         <Header />
+         <div className='flex flex-col my-10'>
+           {weatherData.length === 0 ?
+             <div className='container p-4 flex items-center justify-center h-1/3 mb-auto'>
+              <h1 className='text-gray-300 text-4xl font-bold uppercase'>{noData}</h1>
+             </div> : 
+             <>
+             <h1 className='text-5xl text-gray-800 mt-auto mb-4'>Today</h1>
+             <DetailCard weather_icon={weatherIcon} data={weatherData} />
+             <h1 className='text-3xl text-gray-600 mb-4 mt-10'>More On {City}</h1>
+             <ul className='grid grid-cols-2 gap-2'>
+               {weatherData.list.map( (days, index) => {
+                 if(index > 0) {
+                   return (<SummaryCard key={index} day={days}/>)
+                 }
+               } )}
+             </ul>
+             </>
+           }
+         </div>
        </div>
       </div>
     </div>
